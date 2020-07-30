3914e27b42430abf50478ba638611cd199ddebbc
aaqib90 <mailmeaaqib@gmail.com>
2020-07-30 15:22:34 +0530

    angular + quarkus integration done

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) {
+
+  }
   title = 'webapp';
+  name: string;
+  nameLength;
+  changeInput(e) {
+    console.log(e.target.value);
+    this.name = e.target.value;
+    this.http.get(`https://quarkus-ui.herokuapp.com/hello/string-length/${this.name}`)
+    .pipe(first())
+    .subscribe(
+      (resp) => {
+        console.log(resp);
+        this.nameLength = resp;
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+  }
 }
