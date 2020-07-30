b81a01ad311cca3ba4e853ce25d5effa1e93fbc5
aaqib90 <mailmeaaqib@gmail.com>
2020-07-30 13:51:24 +0530

    Quarkus issue fixed

--- src/main/java/tech/donau/quarkify/GreetingResource.java
+++ src/main/java/tech/donau/quarkify/GreetingResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
 
 
@@ -19,6 +20,6 @@ public class GreetingResource {
     @GET
     @Path("/string-length/{name}")
     public Response stringLength(@PathParam(value ="name") String name) {
-        Response.ok(name.length()).build();
+        return Response.ok(name.length()).build();
     }
 }
